6f60ee144852aa619b05770ce41d8209c0836ba1
Peter Svenningsson <peter.o.svenningsson@gmail.com>
2023-08-12 17:58:43 +0200

    JointHypothesis: add isValid method

--- src/types/src/JointHypothesis.cpp
+++ src/types/src/JointHypothesis.cpp
@@ -2,15 +2,61 @@
 
 JointHypothesis::JointHypothesis(const std::vector<Hypothesis>& hypotheses)
     : hypotheses_(hypotheses), weight_(0) {
+    std::sort(hypotheses_.rbegin(), hypotheses_.rend());
+
     for (auto hypothesis : hypotheses) {
         weight_ += hypothesis.weight();
     }
 }
 
-bool JointHypothesis::operator<(const Hypothesis& rhs) const {
-    return weight_ < rhs.weight_;
+/**
+ * A Joint hypothesis is valid iff two tracks are not associated to the same
+ * detection.
+ *
+ * @returns true if the JointHypothesis is valid.
+ */
+bool JointHypothesis::isValid(void) const {
+    std::vector<Detection> detections;
+    for (auto hypothesis : hypotheses_) {
+        std::optional<Detection> detection = hypothesis.detection();
+        if (detection.has_value()) {
+            detections.push_back(*detection);
+        }
+    }
+
+    return CheckUniqueness(detections);
 }
 
-bool JointHypothesis::operator>(const Hypothesis& rhs) const {
-    return weight_ < rhs.weight_;
+/**
+ * Checks if the elements in the std::vector<Detections> are unique.
+ *
+ * @param detections A vector of detections.
+ * @returns true if elements in detections are unique.
+ */
+bool JointHypothesis::CheckUniqueness(
+    std::vector<Detection>& detections) const {
+    std::sort(detections.begin(), detections.end());
+
+    auto it = std::adjacent_find(detections.begin(), detections.end());
+    if (it != detections.end()) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+bool JointHypothesis::operator<(const JointHypothesis& rhs) const {
+    return weight() < rhs.weight();
+}
+
+bool JointHypothesis::operator>(const JointHypothesis& rhs) const {
+    return weight() < rhs.weight();
+}
+
+bool JointHypothesis::operator==(const JointHypothesis& rhs) const {
+    return weight() < rhs.weight();
+}
+
+double JointHypothesis::weight(void) const {
+    return weight_;
 }
---
+++ test/types/JointHypothesis_test.cpp
@@ -0,0 +1,60 @@
+#include "JointHypothesis.h"
+
+#include <gtest/gtest.h>
+
+#include "Detection.h"
+#include "Track.h"
+
+TEST(JointHypothesisTest, ConstructorAndWeight) {
+    Track track1{Detection(Eigen::Vector3d(1, 1, 1))};
+    Track track2{Detection(Eigen::Vector3d(2, 2, 2))};
+    Detection det1{Eigen::Vector3d(1, 1, 1)};
+    Detection det2{Eigen::Vector3d(2, 2, 2)};
+
+    Hypothesis hyp1{track1, det1, 0.9};
+    Hypothesis hyp2{track2, det2, 0.8};
+
+    std::vector<Hypothesis> hypotheses = {hyp1, hyp2};
+    JointHypothesis jointHypo(hypotheses);
+
+    EXPECT_NEAR(jointHypo.weight(), 1.7, 1e-6);
+}
+
+TEST(JointHypothesisTest, IsNotValid) {
+    Eigen::Vector3d vec1{1.0, 2.0, 3.0};
+    Eigen::Vector3d vec2{2.0, 1.0, 3.0};
+    Track track1{Detection(vec1)};
+    Track track2{Detection(vec2)};
+    Detection detection{vec1};
+
+    Hypothesis hypo1(track1, detection, 0.8);
+    Hypothesis hypo2(track2, detection, 0.8);
+
+    std::vector<Hypothesis> hypotheses;
+    hypotheses.push_back(hypo1);
+    hypotheses.push_back(hypo2);
+
+    JointHypothesis jointHypo(hypotheses);
+    bool isValid = jointHypo.isValid();
+    ASSERT_FALSE(isValid);
+}
+
+TEST(JointHypothesisTest, IsValid) {
+    Eigen::Vector3d vec1{1.0, 2.0, 3.0};
+    Eigen::Vector3d vec2{2.0, 1.0, 3.0};
+    Track track1{Detection(vec1)};
+    Track track2{Detection(vec2)};
+    Detection detection1{vec1};
+    Detection detection2{vec2};
+
+    Hypothesis hypo1(track1, detection1, 0.8);
+    Hypothesis hypo2(track2, detection2, 0.8);
+
+    std::vector<Hypothesis> hypotheses;
+    hypotheses.push_back(hypo1);
+    hypotheses.push_back(hypo2);
+
+    JointHypothesis jointHypo(hypotheses);
+    bool isValid = jointHypo.isValid();
+    ASSERT_TRUE(isValid);
+}
